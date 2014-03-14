cf72a9309394eb3265a26a273a112ca594ff1d58
josecols <josecolsg@gmail.com>
2014-03-14 19:03:23 -0430

    Cambiado el contenido de <seccion> a markdown

--- main/admin.py
+++ main/admin.py
@@ -1,25 +1,47 @@
-# encoding:utf-8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # Sonrisatrol - Django 1.4 - Python 2.7.3
 # Desarrollado por José Cols - josecolsg@gmail.com - @josecols
+
 from django.contrib import admin
-from models import Seccion, Etiqueta, Medalla, Rango, Publicacion, PerfilUsuario
+from models import Seccion, Etiqueta, Medalla, Rango, Publicacion, \
+    PerfilUsuario
+from forms import SeccionForm
+
 
 class PublicacionAdmin(admin.ModelAdmin):
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+
+    def formfield_for_foreignkey(
+        self,
+        db_field,
+        request,
+        **kwargs
+        ):
+
         if db_field.name == 'autor':
             kwargs['initial'] = request.user.id
             return db_field.formfield(**kwargs)
-        return super(PublicacionAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
-        
+        return super(PublicacionAdmin,
+                     self).formfield_for_foreignkey(db_field, request,
+                **kwargs)
+
     list_display = ('titulo', 'favoritos', 'aprobado')
     search_fields = ('titulo', 'etiquetas')
-    list_filter = ('aprobado',)
-        
+    list_filter = ('aprobado', )
+
+
 class SeccionAdmin(admin.ModelAdmin):
+
+    form = SeccionForm
+
     class Media:
-        js = ('/static/js/tiny_mce/tiny_mce.js',
-              '/static/js/tiny_mce/basic_config.js',)
-        
+
+        css = {'all': ('/static/js/ghost/ghostdown.css', )}
+        js = ('/static/js/jquery-1.10.2.min.js',
+              '/static/js/ghost/ghostdown.js')
+
+
 admin.site.register(Seccion, SeccionAdmin)
 admin.site.register(Etiqueta)
 admin.site.register(Medalla)
--- main/forms.py
+++ main/forms.py
@@ -1,82 +1,148 @@
-# encoding:utf-8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # Sonrisatrol - Django 1.4 - Python 2.7.3
 # Desarrollado por José Cols - josecolsg@gmail.com - @josecols
+
 from django import forms
+from django.forms import widgets
 from django.contrib.auth.models import User
-from models import Publicacion, PerfilUsuario
+from django.utils.safestring import mark_safe
+from models import Publicacion, PerfilUsuario, Seccion
 from django.contrib.auth.forms import UserCreationForm
-     
+
+
 class RegistroUsuarioForm(UserCreationForm):
+
     email = forms.EmailField(required=True)
+
     class Meta:
+
         model = User
-        fields = ("username", "email", "password1", "password2") 
-        
+        fields = ('username', 'email', 'password1', 'password2')
+
     def save(self, commit=True):
         user = super(RegistroUsuarioForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
+        user.email = self.cleaned_data['email']
         if commit:
             user.save()
-        return user   
+        return user
 
 
 class PublicarImagenForm(forms.ModelForm):
+
     class Meta:
+
         model = Publicacion
-        fields = ("titulo", "imagen", "etiquetas") 
-        
+        fields = ('titulo', 'imagen', 'etiquetas')
+
     def __init__(self, *args, **kwargs):
         self._autor = kwargs.pop('autor')
-        
+
         super(PublicarImagenForm, self).__init__(*args, **kwargs)
+
     def save(self, commit=True):
         publicacion = super(PublicarImagenForm, self).save(commit=False)
         publicacion.autor = self._autor
         super_save_m2m = self.save_m2m
+
         def save_m2m():
             super_save_m2m()
             publicacion.etiquetas.clear()
             for etiqueta in self.cleaned_data['etiquetas']:
                 publicacion.etiquetas.add(etiqueta)
+
         self.save_m2m = save_m2m
         if commit:
             publicacion.save()
             self.save_m2m()
-        return publicacion   
-        
-        
+        return publicacion
+
+
 class PublicarVideoForm(forms.ModelForm):
+
     class Meta:
+
         model = Publicacion
-        fields = ("titulo", "video", "etiquetas")
-        
+        fields = ('titulo', 'video', 'etiquetas')
+
     def __init__(self, *args, **kwargs):
         self._autor = kwargs.pop('autor')
         super(PublicarVideoForm, self).__init__(*args, **kwargs)
-        
+
     def save(self, commit=True):
         publicacion = super(PublicarVideoForm, self).save(commit=False)
         publicacion.autor = self._autor
         super_save_m2m = self.save_m2m
+
         def save_m2m():
             super_save_m2m()
             publicacion.etiquetas.clear()
             for etiqueta in self.cleaned_data['etiquetas']:
                 publicacion.etiquetas.add(etiqueta)
+
         self.save_m2m = save_m2m
         if commit:
             publicacion.save()
             self.save_m2m()
-        return publicacion   
+        return publicacion
 
 
 class UsuarioForm(forms.ModelForm):
+
     class Meta:
+
         model = User
-        fields = ('first_name', 'last_name', 'email')  
-        
-        
+        fields = ('first_name', 'last_name', 'email')
+
+
 class PerfilUsuarioForm(forms.ModelForm):
+
     class Meta:
+
         model = PerfilUsuario
-        fields = ('avatar', 'acerca', 'twitter', 'facebook')  
+        fields = ('avatar', 'acerca', 'twitter', 'facebook')
+
+
+class GhostWidget(widgets.Textarea):
+
+    def render(
+        self,
+        name,
+        value,
+        attrs=None,
+        ):
+
+        return mark_safe(u'''
+                <div class="editorwrap">
+                    <section class="entry-markdown">
+                        <header class="floatingheader">
+                            &nbsp;&nbsp; Markdown
+                        </header>
+                        <section class="entry-markdown-content">
+                            %s
+                        </section>
+                    </section>
+                    <section class="entry-preview active">
+                        <header class="floatingheader">
+                            &nbsp;&nbsp; Resultado
+                            <span class="entry-word-count">0 words</span>
+                        </header>
+                        <section class="entry-preview-content">
+                            <div class="rendered-markdown"></div>
+                        </section>
+                    </section>
+                </div>
+                '''
+                         % super(GhostWidget, self).render(name, value,
+                         attrs={'id': 'entry-markdown'}))
+
+
+class SeccionForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Seccion
+        widgets = {'contenido': GhostWidget}
+
+
