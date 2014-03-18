502f0dfa8b252aa36e278cae64830990b6cbd652
josecols <josecolsg@gmail.com>
2014-03-17 21:52:49 -0430

    Cambiada categoría por defecto de <popular> a <nuevo>

--- main/views.py
+++ main/views.py
@@ -22,13 +22,13 @@ from forms import RegistroUsuarioForm, PublicarImagenForm, PublicarVideoForm, Us
 PUBLICACIONES = 10
 
 def paginar(lista, pagina, items):
-    paginator = Paginator(lista, items)    
+    paginator = Paginator(lista, items)
     try:
         publicaciones = paginator.page(pagina)
     except PageNotAnInteger:
         publicaciones = paginator.page(1)
     except EmptyPage:
-        publicaciones = paginator.page(paginator.num_pages)     
+        publicaciones = paginator.page(paginator.num_pages)
     return publicaciones
 
 def asignar_medalla(usuario, titulo):
@@ -40,70 +40,70 @@ def aviso(request, mensaje, exito):
     request.session['mensaje'] = mensaje
     request.session['exito_mensaje'] = exito
     return request
-    
+
 @csrf_protect
 def favorito(request):
     if request.method == 'POST':
-        publicacion = get_object_or_404(Publicacion, pk=request.POST.get('publicacion_id', None))   
-        usuario = PerfilUsuario.objects.get(user=request.user)    
-        autor = PerfilUsuario.objects.get(user=publicacion.autor) 
-         
-        if publicacion in usuario.favoritos.all():      
-            autor.puntos -= 1      
-            publicacion.favoritos -= 1            
+        publicacion = get_object_or_404(Publicacion, pk=request.POST.get('publicacion_id', None))
+        usuario = PerfilUsuario.objects.get(user=request.user)
+        autor = PerfilUsuario.objects.get(user=publicacion.autor)
+
+        if publicacion in usuario.favoritos.all():
+            autor.puntos -= 1
+            publicacion.favoritos -= 1
             usuario.favoritos.remove(publicacion)
         else:
             autor.puntos += 1
-            publicacion.favoritos += 1            
+            publicacion.favoritos += 1
             usuario.favoritos.add(publicacion)
-                           
-        publicacion.save()                
+
+        publicacion.save()
         usuario.save()
-        autor.save()       
-        
+        autor.save()
+
         # Medalla
-        if usuario.favoritos.all().count() == 1000:            
+        if usuario.favoritos.all().count() == 1000:
             try:
                 medalla = Medalla.objects.get(titulo="Sonrisa de guasón")
             except Rango.DoesNotExist:
-                medalla = None    
+                medalla = None
             if medalla:
                 usuario.medallas.add(medalla)
-                
+
         return HttpResponse(simplejson.dumps(filters.cuenta(publicacion.favoritos)), mimetype='application/javascript')
     raise Http404
-        
-def index(request, categoria="popular", pagina="1", mensaje=None):        
+
+def index(request, categoria="nuevo", pagina="1", mensaje=None):
     if categoria == "popular":
         lista = Publicacion.objects.filter(aprobado=True).order_by('favoritos').reverse()
     elif categoria == 'nuevo':
-        lista = Publicacion.objects.filter(aprobado=True).order_by('fecha').reverse()        
+        lista = Publicacion.objects.filter(aprobado=True).order_by('fecha').reverse()
     else:
         raise Http404
-    
-    publicaciones = paginar(lista, pagina, PUBLICACIONES)    
+
+    publicaciones = paginar(lista, pagina, PUBLICACIONES)
     return render_to_response('index.html',
                               {'publicaciones':publicaciones, 'categoria':categoria, 'request':request},
                               context_instance=RequestContext(request))
 
 def usuario(request, username, categoria="aportes", pagina="1"):
     user = get_object_or_404(User, username=username)
-    perfil = PerfilUsuario.objects.get(user=user)        
+    perfil = PerfilUsuario.objects.get(user=user)
     if categoria == "aportes":
         lista = Publicacion.objects.filter(autor=user, aprobado=True).order_by('fecha').reverse()
         favoritos = None
-    elif categoria == "favoritos": 
+    elif categoria == "favoritos":
         lista = perfil.favoritos.all()
         favoritos = not None
     else:
         raise Http404
-    
+
     publicaciones = paginar(lista, pagina, PUBLICACIONES)
     return render_to_response('usuario.html',
                               {'perfil':perfil, 'publicaciones':publicaciones, 'favoritos':favoritos, 'request':request},
                               context_instance=RequestContext(request))
 
-def seccion(request, slug):    
+def seccion(request, slug):
     seccion = get_object_or_404(Seccion, slug=slug)
     return render_to_response('seccion.html',
                               {'seccion':seccion, 'request':request},
@@ -114,7 +114,7 @@ def publicacion(request, slug, post_id):
     return render_to_response('publicacion.html',
                               {'publicacion':publicacion, 'request':request},
                               context_instance=RequestContext(request))
-    
+
 def busqueda(request, pagina="1", query=None):
     if not query:
         query = request.GET.get('q', '')
@@ -124,7 +124,7 @@ def busqueda(request, pagina="1", query=None):
             resultados = paginar(lista, pagina, PUBLICACIONES * 2)
         else:
             resultados = None
-            
+
     return render_to_response('busqueda.html',
                               {'resultados': resultados, 'query': query, 'request':request},
                               context_instance=RequestContext(request))
@@ -133,23 +133,23 @@ def busqueda(request, pagina="1", query=None):
 @login_required(login_url='/autenticacion')
 def publicar(request, tipo_publicacion="imagen"):
     if tipo_publicacion == "imagen":
-        if request.method == 'POST':        
+        if request.method == 'POST':
                 formulario = PublicarImagenForm(request.POST, request.FILES, autor=request.user)
                 if formulario.is_valid():
                     formulario.save()
                     return redirect('index')
         else:
             formulario = PublicarImagenForm(autor=request.user)
-    
+
     else:
-        if request.method == 'POST':        
+        if request.method == 'POST':
             formulario = PublicarVideoForm(request.POST, autor=request.user)
             if formulario.is_valid():
                 formulario.save()
                 return redirect('index')
         else:
             formulario = PublicarVideoForm(autor=request.user)
-        
+
     return render_to_response('publicar.html',
                               {'formulario':formulario, 'request':request},
                               context_instance=RequestContext(request))
@@ -157,7 +157,7 @@ def publicar(request, tipo_publicacion="imagen"):
 @login_required(login_url='/autenticacion')
 def editar_perfil(request):
     usuario = PerfilUsuario.objects.get(user=request.user)
-    
+
     if request.method == 'POST':
         usuario_form = UsuarioForm(request.POST, instance=request.user)
         perfil_form = PerfilUsuarioForm(request.POST, request.FILES, instance=request.user.perfil)
@@ -173,11 +173,11 @@ def editar_perfil(request):
     return render_to_response('editar-perfil.html',
                               { 'usuario_form': usuario_form, 'perfil_form': perfil_form, 'request':request },
                               context_instance=RequestContext(request))
-        
+
 def registro(request):
     if request.user.is_authenticated():
         return redirect('index')
-    
+
     if request.method == 'POST':
         formulario = RegistroUsuarioForm(request.POST)
         if formulario.is_valid():
@@ -186,29 +186,29 @@ def registro(request):
             return redirect('index')
     else:
         formulario = RegistroUsuarioForm()
-        
+
     return render_to_response('registro.html',
                               {'formulario':formulario},
                               context_instance=RequestContext(request))
-    
+
 def autenticacion(request, error=None):
     if request.user.is_authenticated():
         return redirect('index')
-    
+
     if request.method == 'POST':
         formulario = AuthenticationForm()
         usuario = authenticate(username=request.POST['username'], password=request.POST['password'])
         if usuario is not None:
             if usuario.is_active:
-                login(request, usuario)                
+                login(request, usuario)
             else:
                 aviso(request, '¡Error! El usuario se encuentra desactivado', False)
             return redirect('index')
         else:
             return render_to_response('autenticacion.html',
                                       {'formulario':formulario, 'error':'¡Error! El usuario y la contraseña no coinciden'},
-                                      context_instance=RequestContext(request))        
-                
+                                      context_instance=RequestContext(request))
+
     formulario = AuthenticationForm()
     return render_to_response('autenticacion.html',
                               {'formulario':formulario},
@@ -216,7 +216,7 @@ def autenticacion(request, error=None):
 
 @csrf_protect
 def borrar_mensaje(request):
-    if request.method == 'POST': 
+    if request.method == 'POST':
         request.session['mensaje'] = request.session['exito_mensaje'] = None
 
 def salir(request):
@@ -224,7 +224,7 @@ def salir(request):
     return redirect('index')
 
 # Google webmaster tools
-def google(request):          
+def google(request):
     return render_to_response('google7076d88b2720fcc5.html',
                               {'request':request},
                               context_instance=RequestContext(request))
